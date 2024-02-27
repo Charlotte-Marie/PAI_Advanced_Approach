9dae9835025d5b7e68d349d0da5ee386c5ba65df
beccadelf <rebecca.delfendahl@student.hu-berlin.de>
2024-02-27 17:45:46 +0100

    html update

--- Html_output/html_script.py
+++ Html_output/html_script.py
@@ -6,15 +6,19 @@ from jinja2 import Template
 
 # Data for the template
 template_data = {
-    'title': 'Your Plots',
-    'heading': 'Your Plots and Text',
-    'plot_filenames': ["C:\\Users\\charl\\Documents\\Promotion\\results_synth\\random_forest\\plots\\optimal_vs_nonoptimal_0_all.png",
-                       "C:\\Users\\charl\\Documents\\Promotion\\results_synth\\random_forest\\plots\\optimal_vs_nonoptimal_1_all.png"],
+    'title': 'PAI Report',
+    'main_heading': 'Report Personalized Advantage Index',
+    'sub_header_1': 'Evaluating the PAI across 100 repetitions of the CV',
+    'sub_header_2': 'Evaluating the PAI – results per repetition',
+    #'plot_filenames': ["C:\\Users\\charl\\Documents\\Promotion\\results_synth\\random_forest\\plots\\optimal_vs_nonoptimal_0_all.png",
+    #                   "C:\\Users\\charl\\Documents\\Promotion\\results_synth\\random_forest\\plots\\optimal_vs_nonoptimal_1_all.png"],
+    'plot_filenames': ["C:\\Users\\Acer\\Documents\\GitHub\\PAI_Advanced_Approach\\Html_output\\results_test_data\\ridge_regression\\plots\\PAI_distribution_0_all",
+                       "C:\\Users\\Acer\\Documents\\GitHub\\PAI_Advanced_Approach\\Html_output\\results_test_data\\ridge_regression\\plots\\optimal_vs_nonoptimal_0_all.png"], 
     'additional_text': 'Additional text goes here.',
 }
 
 # Read the template file
-with open('html_template_click.html', 'r') as template_file:
+with open('html_template_click_RD.html', 'r') as template_file:
     template_content = template_file.read()
 
 # Create a Jinja2 template
