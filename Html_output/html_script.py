c445b82d8cca95c232f861949fc25e3fad07215d
beccadelf <rebecca.delfendahl@student.hu-berlin.de>
2024-02-28 13:11:27 +0100

    Update RD

--- Html_output/html_script.py
+++ Html_output/html_script.py
@@ -10,9 +10,10 @@ template_data = {
     'main_heading': 'Report Personalized Advantage Index',
     'sub_header_1': 'Evaluating the PAI across 100 repetitions of the CV',
     'sub_header_2': 'Evaluating the PAI – results per repetition',
+    'sub_header_3': 'Model performance across 100 x 5-fold CV',
     #'plot_filenames': ["C:\\Users\\charl\\Documents\\Promotion\\results_synth\\random_forest\\plots\\optimal_vs_nonoptimal_0_all.png",
     #                   "C:\\Users\\charl\\Documents\\Promotion\\results_synth\\random_forest\\plots\\optimal_vs_nonoptimal_1_all.png"],
-    'plot_filenames': ["C:\\Users\\Acer\\Documents\\GitHub\\PAI_Advanced_Approach\\Html_output\\results_test_data\\ridge_regression\\plots\\PAI_distribution_0_all",
+    'plot_filenames': ["C:\\Users\\Acer\\Documents\\GitHub\\PAI_Advanced_Approach\\Html_output\\results_test_data\\ridge_regression\\plots\\PAI_distribution_0_all.png",
                        "C:\\Users\\Acer\\Documents\\GitHub\\PAI_Advanced_Approach\\Html_output\\results_test_data\\ridge_regression\\plots\\optimal_vs_nonoptimal_0_all.png"], 
     'additional_text': 'Additional text goes here.',
 }
