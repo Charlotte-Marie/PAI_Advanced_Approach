65cdf8f6aa4d446cb80ca6526872ad150eae4cf1
Till Julius Adam <133876003+TillJAdam@users.noreply.github.com>
2024-03-02 17:22:11 +0100

    Update html_script.py

--- Html_output/html_script.py
+++ Html_output/html_script.py
@@ -1,8 +1,20 @@
 from jinja2 import Template
+import os
 
 # Set paths
-summary_path = "C:/Users/tillj/Documents/GitHub/PAI_Advanced_Approach/Html_output/results_test_data/ridge_regression/PAI_summary_all.txt"
-model_performance_path = "C:/Users/tillj/Documents/GitHub/PAI_Advanced_Approach/Html_output/results_test_data/ridge_regression/modelperformance_summary.txt"
+summary_path = "Euer Path zu: PAI_summary_all.txt"
+model_performance_path = "Euer Path zu: modelperformance_summary.txt"
+plots_directory = "Euer Path zu: ridge_regression/plots"
+
+# Organizing files in "plots"
+plot_files = os.listdir(plots_directory)
+pai_distribution_files = [file for file in plot_files if file.startswith("PAI_distribution")]
+optimal_vs_nonoptimal_files = [file for file in plot_files if file.startswith("optimal_vs_nonoptimal")]
+# Sorting files in "plots"
+pai_distribution_files.sort()
+optimal_vs_nonoptimal_files.sort()
+pai_distribution_files = [os.path.join(plots_directory, file) for file in pai_distribution_files]
+optimal_vs_nonoptimal_files = [os.path.join(plots_directory, file) for file in optimal_vs_nonoptimal_files]
 
 # Read the PAI summary file
 with open(summary_path, 'r') as summary_file:
@@ -10,18 +22,18 @@ with open(summary_path, 'r') as summary_file:
 
 # Extract column names and values
 column_names = lines[0].strip().split('\t')
-summary_values = lines[1].strip().split('\t')[1:]
+summary_values = lines[1].strip().split('\t')[1:] # skip first value (row index)
 
 # Read and process the model performance summary file
 model_performance_data = {}
 with open(model_performance_path, 'r') as file:
     for line in file:
-        parts = line.strip().split('\t')
-        if len(parts) == 2:
+        parts = line.strip().split('\t')  # Split the line by tab
+        if len(parts) == 2:  # Check if the line contains exactly two values
             key, value = parts
             model_performance_data[key] = float(value)
 
-# Model performance metrics for HTML template
+# Extracting specific metrics for the HTML template
 model_performance_metrics = {
     'Mean_MSE': [
         model_performance_data['Mean_all_RMSE'],
@@ -49,16 +61,17 @@ template_data = {
     'sub_header_3': 'Model performance across 100 x 5-fold CV',
     'column_names': column_names,
     'summary_values': summary_values,
+    'model_performance_metrics': model_performance_metrics,
     'figure_1_title': 'Distribution of the absolute PAI',
     'figure_2_title': 'Distribution of outcome optimal and nonoptimal',
-    #'plot_filenames': ["C:\\Users\\charl\\Documents\\Promotion\\results_synth\\random_forest\\plots\\optimal_vs_nonoptimal_0_all.png",
-    #                   "C:\\Users\\charl\\Documents\\Promotion\\results_synth\\random_forest\\plots\\optimal_vs_nonoptimal_1_all.png"],
-    'plot_filenames': ["C:\\Users\\Acer\\Documents\\GitHub\\PAI_Advanced_Approach\\Html_output\\results_test_data\\ridge_regression\\plots\\PAI_distribution_0_all.png",
-                       "C:\\Users\\Acer\\Documents\\GitHub\\PAI_Advanced_Approach\\Html_output\\results_test_data\\ridge_regression\\plots\\optimal_vs_nonoptimal_0_all.png"], 
+    'plot_filenames': {
+        'pai_distribution': pai_distribution_files,
+        'optimal_vs_nonoptimal': optimal_vs_nonoptimal_files,
+    },
     'additional_text': 'Additional text goes here.',
 }
 
-# Read the template file
+
 with open('html_template_click_RD.html', 'r') as template_file:
     template_content = template_file.read()
 
@@ -67,6 +80,10 @@ template = Template(template_content)
 
 # Render the template with data
 html_content = template.render(template_data)
+
+# Save HTML content to a file
+with open('output.html', 'w') as f:
+    f.write(html_content)
 
 # Save HTML content to a file
 with open('output.html', 'w') as f:
